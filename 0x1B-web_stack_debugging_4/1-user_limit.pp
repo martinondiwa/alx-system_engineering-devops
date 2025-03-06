f91c905ab26a0c7c288c1848212d60ca937aca02
martinondiwa <surekondiwa@gmail.com>
2025-03-06 18:57:57 +0000

    sdfg

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,2 +1,12 @@
- puppet script
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
+# Reconfigure the OS for 'holberton' to login and open a file without any error message
+
+exec { 'increase-hard-file-limit-holberton-user':
+  command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
